94cc63445183702f70ea7fc977dbc45b2151aa5d
Megha Krishna <97619961+megkrish@users.noreply.github.com>
2025-06-25 17:07:51 +0530

    Delete menu_analyzer_project directory

--- menu_analyzer_project/menu_analyzer/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Restaurant, Menu
-
-admin.site.register(Restaurant)
-admin.site.register(Menu)
--- menu_analyzer_project/menu_analyzer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('compare/', views.compare_menus, name='compare_menus'),  # Ensure this URL path is correct
-]
